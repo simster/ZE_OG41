ce0ab12c9aefa0fcb0718d7698a790ccfc546958
marcolamm <marcolamm68@gmail.com>
2013-01-12 12:49:40 +0100

    NeueDurchsuchenKlasse

---
+++ src/main/java/ch/sipama/Funktionen/StringDurchsuchen.java
@@ -0,0 +1,32 @@
+package ch.sipama.Funktionen;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/*
+ * Marco 12.1.13
+ * Methode um URL-String aus URLAuslesenFile.java zu durchsuchen
+ */
+
+public class StringDurchsuchen {
+
+	final static String urlString = ("quelltext.txt");// Pfad muss noch mit Patrizia bestimmt werden
+
+	public void attributeAuslesen() throws FileNotFoundException{
+try{
+		BufferedReader bReader = new BufferedReader(new FileReader(urlString));
+
+		String sZeile = bReader.readLine();
+		while (sZeile != null) 
+		{
+			sZeile = bReader.readLine();
+
+			System.out.println(sZeile);
+
+		}
+	} catch(IOException ioe){
+		 ioe.printStackTrace();
+	}
+}}

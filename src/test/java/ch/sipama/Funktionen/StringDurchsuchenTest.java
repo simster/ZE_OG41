7d5b04ca3b25945c48578b1167370dd2c19ac217
marcolamm <marcolamm68@gmail.com>
2013-01-13 16:44:02 +0100

    Testklasse StringDurchsuchen

---
+++ src/test/java/ch/sipama/Funktionen/StringDurchsuchenTest.java
@@ -0,0 +1,13 @@
+package ch.sipama.Funktionen;
+
+import org.junit.*;
+
+public class StringDurchsuchenTest {
+
+	@Test
+	public void stringDurchsuchenErstellen() {
+		StringDurchsuchen durchsuchen = new StringDurchsuchen();
+
+	}
+
+}

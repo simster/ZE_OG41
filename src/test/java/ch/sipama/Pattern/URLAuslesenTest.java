e1f1e179997ccc98e7be9aa44c37581881ee149c
Dreyer Patrizia <titz@gmx.ch>
2013-01-14 21:55:43 +0100

    Quelltext in Textfeld ausgeben

---
+++ src/test/java/ch/sipama/Pattern/URLAuslesenTest.java
@@ -0,0 +1,16 @@
+package ch.sipama.Pattern;
+
+import org.junit.Test;
+
+
+/** 
+ * Auslesen des Sourcecodes testen 
+ */
+public class URLAuslesenTest {
+	
+	@Test
+	public void testAuslesen() {
+		assert ( true );
+	}
+
+}

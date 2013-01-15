42c1558a7613b7c8c77168ae56166843bf51c10e
marcolamm <marcolamm68@gmail.com>
2013-01-15 13:44:09 +0100

    TextfileVon URL

--- src/main/java/ch/sipama/Funktionen/StringDurchsuchen.java
+++ src/main/java/ch/sipama/Funktionen/StringDurchsuchen.java
@@ -1,22 +1,58 @@
 package ch.sipama.Funktionen;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 /*
  * @author Marco 
- * 12.1.13
+ * 15.1.13
  * Methode um URL-String aus URLAuslesenFile.java zu durchsuchen
  */
 
 public class StringDurchsuchen {
 
 	// Pfad muss noch mit Patrizia bestimmt werden
-	final static String urlString = ("C:/Users/marcolamm/Documents/GitHub/ZE_OG41/docs/quelltexte/quelltext.txt");
 
-	public void attributeAuslesen() throws FileNotFoundException {
+	// while ( ( strLine = br.readLine() ) != null)
+	// System.out.println(strLine);
+	// br.close();
+
+	// final static String urlString =
+	// ("C:/Users/marcolamm/Documents/GitHub/ZE_OG41/docs/quelltexte/quelltext.txt");
+	// final static String urlString =
+	// ("https://github.com/simster/ZE_OG41/blob/master/docs/quelltexte/quelltext.txt");
+
+	public void attributeAuslesen() {
+		URL llll = null;
+
+		try {
+			llll = new URL(
+					"http://github.com/simster/ZE_OG41/blob/master/docs/quelltexte/quelltext.txt");
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		URLConnection conLLL = null;
+		try {
+			conLLL = llll.openConnection();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					conLLL.getInputStream()));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String urlString = "";
+
 		try {
 			BufferedReader bReader = new BufferedReader(new FileReader(
 					urlString));
@@ -28,7 +64,7 @@ public class StringDurchsuchen {
 				if (sZeile.contains("blockfoot actionbuttons")) {
 					System.out.println(sZeile);
 				} else {
-//					System.out.println("Enthält gesuchten String NICHT!");
+					System.out.println("Enthält gesuchten String NICHT!");
 				}
 			}
 		} catch (IOException ioe) {
